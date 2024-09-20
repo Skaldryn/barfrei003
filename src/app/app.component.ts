808cd4cda9a019e26a8dcd383e1f0245da658c8e
Alice Boeddinghaus <s0587021@htw-berlin.de>
2024-09-20 20:53:56 +0200

    saving

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {NavigationEnd, Router, RouterOutlet} from '@angular/router';
 import {NavComponent} from "./nav/nav.component";
 import {FooterComponent} from "./footer/footer.component";
 
@@ -12,4 +12,21 @@ import {FooterComponent} from "./footer/footer.component";
 })
 export class AppComponent {
   title = 'BarFreiFrontend002';
+
+/*
+  currentUrl: string = '';
+
+  constructor(private router: Router) {
+    // Subscribe to router events to capture the current URL
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Set the current URL, ignoring query parameters or fragments
+        this.currentUrl = event.urlAfterRedirects.split('#')[0];
+      }
+    });
+  }
+
+ */
+
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -33,7 +33,7 @@ export const routes: Routes = [
 
    // {
    // anchorScrolling: 'enabled', // Enables anchor scrolling
- // }
+  //}
 
   ), ReactiveFormsModule],
 
