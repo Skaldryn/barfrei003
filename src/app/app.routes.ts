760370d1686cf483a10bf76853c57301a6ea450c
Alice Boeddinghaus <s0587021@htw-berlin.de>
2024-09-20 15:25:04 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,43 @@
-import { Routes } from '@angular/router';
+//import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AusstellungenComponent } from './ausstellungen/ausstellungen.component';
+import { KontaktComponent } from './kontakt/kontakt.component';
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {ReactiveFormsModule} from "@angular/forms";
+
+
+
+export const routes: Routes = [
+
+  {path: "kontakt",
+    component: KontaktComponent},
+
+  {path: "ausstellungen",
+    component: AusstellungenComponent},
+
+  {path: "",
+    component: HomeComponent,
+    pathMatch: 'full'}
+
+
+
+];
+
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes,
+
+   // {
+   // anchorScrolling: 'enabled', // Enables anchor scrolling
+ // }
+
+  ), ReactiveFormsModule],
+
+
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
